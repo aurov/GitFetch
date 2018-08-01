60942923ff431685502c3936f75416059a13e171
aurov <urov.art@gmail.com>
2018-08-01 20:23:28 +0300

    add titles

---
+++ src/components/Titles.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Titles = props => (
+  <div className="main__text-container">
+    <h1 className="main__title">GitFetch - Profile Finder <br/> for GitHub</h1>
+    <h3 className="main__subtitle">Check out the repos, url and more,
+      <br/> just by entering a username!</h3>
+  </div>
+);
+
+export default Titles;
